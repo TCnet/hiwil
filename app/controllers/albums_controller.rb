5d8163fa5ef92293eb517afd6b537c39c4365519
Hxmusic <hxmusic@foxmail.com>
2017-07-20 13:41:44 +0800

    add albums search

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -9,11 +9,14 @@ class AlbumsController < ApplicationController
  
  
   def index
-    @albums = current_user.albums.paginate(page: params[:page])
-    #@category = category_for current_user.albums
+   # @albums = current_user.albums.paginate(page: params[:page])
+   # @category = category_for current_user.albums
+    @category = category_for current_user.albums
     
+    sql = "name LIKE ?"
     #condition = params[:q].nil? "":"name like \%"+params[:q]+"\%"
-  #  @albums = current_user.albums.search(params[:q]).paginate(page: params[:page])
+    @albums = current_user.albums.where(sql,"%#{params[:q]}%").paginate(page: params[:page])
+    
   end
 
   def new
--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -3,8 +3,9 @@ module Searchable
 
   module ClassMethods
     def search(search, attributes: nil)
-      attributes = self.new.attributes.keys - ["id", "created_at", "updated_at"] if attributes.nil?
-      like_sql = attributes.map { |attr| "#{attr} LIKE ?" }.join("OR ")
+      attributes = self.new.attributes.keys - ["id", "created_at", "updated_at","user_id"] if attributes.nil?
+      
+      like_sql = attributes.map { |attr| "#{attr} LIKE ?" }.join(" OR ")
 
       where(like_sql, *(["%#{search}%"]*attributes.size))
     end
