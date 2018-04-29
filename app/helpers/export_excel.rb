19c2285a7ce7c83e38cdcaabfdffc5941289a0c2
Hxmusic <hxmusic@foxmail.com>
2018-04-29 22:55:54 +0800

    modify albums helper

--- app/helpers/export_excel.rb
+++ app/helpers/export_excel.rb
@@ -5,7 +5,7 @@ module ExportExcel
 
   Spreadsheet.client_encoding = "UTF-8"  
   
-  def outamazon(albums,etemplate,path,user)
+  def outamazon(albums,etemplate,path,user,outfilename)
    
 
     book = Spreadsheet::Workbook.new
@@ -337,7 +337,7 @@ module ExportExcel
         
         code.each_with_index do |f,n|
           csize.each_with_index do |e,m|
-            num = n*csize.length+m+titlecloum
+            num = n*csize.length+m+1
             sn = (num-1)*5+5
 
             sheet1[num+1+c_cloum,t_num] = key_array[sn].join(',')
@@ -361,7 +361,7 @@ module ExportExcel
 
       code.each_with_index do |f,n|
         csize.each_with_index do |e,m|
-          num = n*csize.length+m+titlecloum
+          num = n*csize.length+m+1
           
           #sheet1[num+1,cloum_keywords] = key_array[0].join(',')[0,1000]
           sheet1[num+1+c_cloum,t_num+1] = key_array[0].join(',')[0,1000]
@@ -450,7 +450,7 @@ module ExportExcel
 
 
     #create excel
-    filename = etemplate.name+Time.now.to_s+".xls";
+    filename = outfilename+".xls";
 
     file_path=path+"uploads/export/"+filename
 
@@ -465,5 +465,7 @@ module ExportExcel
     end
     
   end
+
+
   
 end
